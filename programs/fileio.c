1d1c6a82d829d8427da599939057f71067e1bb61
Bimba Shrestha <bimbashrestha@fb.com>
2020-02-03 11:33:14 -0800

    Removing default params and increasing window log

--- programs/fileio.c
+++ programs/fileio.c
@@ -797,11 +797,6 @@ typedef struct {
     ZSTD_CStream* cctx;
 } cRess_t;
 
-#define LDM_PARAM_DEFAULT 9999
-#define DEFAULT_PATCHFROM_LDM_HASH_LOG 7
-#define DEFAULT_PATCHFROM_LDM_MIN_MATCH 64
-#define DEFAULT_PATCHFROM_LDM_BUCKSIZE_LOG 3
-
 static cRess_t FIO_createCResources(FIO_prefs_t* const prefs,
                                     const char* dictFileName, const size_t maxSrcFileSize,
                                     int cLevel, ZSTD_compressionParameters comprParams,
@@ -830,7 +825,8 @@ static cRess_t FIO_createCResources(FIO_prefs_t* const prefs,
             comprParams.windowLog = ADAPT_WINDOWLOG_DEFAULT;
 
         if (prefs->patchFromMode) {
-            comprParams.windowLog = FIO_highbit64((unsigned long long)maxSrcFileSize + PATCHFROM_WINDOWSIZE_EXTRA_BYTES);
+            /* Make sure there is enough room for dict and src in long mode */
+            comprParams.windowLog = FIO_highbit64((unsigned long long)maxSrcFileSize + dictBuffSize + PATCHFROM_WINDOWSIZE_EXTRA_BYTES) + 1;
         }
 
         CHECK( ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_contentSizeFlag, 1) );  /* always enable content size when available (note: supposed to be default) */
@@ -851,13 +847,6 @@ static cRess_t FIO_createCResources(FIO_prefs_t* const prefs,
         }
         if (prefs->ldmHashRateLog != FIO_LDM_PARAM_NOTSET) {
             CHECK( ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_ldmHashRateLog, prefs->ldmHashRateLog) );
-        }
-
-        /* TODO: Need to improve these defaults */
-        if (prefs->patchFromMode && prefs->ldmFlag) {
-            CHECK( ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_ldmHashLog, prefs->ldmHashLog ? prefs->ldmHashLog : DEFAULT_PATCHFROM_LDM_HASH_LOG) );
-            CHECK( ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_ldmMinMatch, prefs->ldmMinMatch ? prefs->ldmMinMatch : DEFAULT_PATCHFROM_LDM_MIN_MATCH) );
-            CHECK( ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_ldmBucketSizeLog, prefs->ldmBucketSizeLog != LDM_PARAM_DEFAULT ? prefs->ldmBucketSizeLog : DEFAULT_PATCHFROM_LDM_BUCKSIZE_LOG) );
         }
 
         /* compression parameters */
