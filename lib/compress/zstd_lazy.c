a78188e58f63b2be7817b837046feb3c06884d39
Bimba Shrestha <bimbashrestha@fb.com>
2020-03-26 08:59:49 -0500

    tp12

--- lib/compress/zstd_lazy.c
+++ lib/compress/zstd_lazy.c
@@ -662,9 +662,11 @@ size_t ZSTD_HcFindBestMatch_generic (
         matchIndex = NEXT_IN_CHAIN(matchIndex, chainMask);
     }
 
-    if (dictMode == ZSTD_dictMatchState)
+    if (dictMode == ZSTD_dictMatchState) {
+        PREFETCH_L1(ms->dictMatchState->chainTable);
         ZSTD_HcFindBestMatch_dictMatchState(offsetPtr, &ml, ms, 
             matchIndex, dictLimit, mls, nbAttempts, iLimit, prefixStart, current, ip);
+    }
 
     return ml;
 }
